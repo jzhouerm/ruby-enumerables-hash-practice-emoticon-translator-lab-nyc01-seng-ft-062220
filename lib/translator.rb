a729cf86d2b3e31c55195a6837982990de0a7d05
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-19 03:38:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,12 +15,24 @@ def load_library(yml)
 
   end
   new_hash #must return new hash
+
+end
+def get_japanese_emoticon (yml, eng_emoticon) #pass through file path and english emoticon
+  library = load_library(yml) #create library variable equal to the load_library helper method created earlier (to convert YML file into hash)
+    library.each do |emotion, emoticon| #iterate over library with emotion as the key and emoticon as the value
+      return emoticon[:japanese] if emoticon[:english] == eng_emoticon #return the japanese emoticon if the associated english emoticon passed through exists in YML file
+    end
+  "Sorry, that emoticon was not found" #error if emoticon does not exist
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
 
-def get_english_meaning
+
+def get_english_meaning(yml, jap_emo)
   # code goes here
+
+  library = load_library(yml)
+    library.each do |eng_emotion, emoticon|
+      return eng_emotion if emoticon[:japanese] == jap_emo
+    end
+  "Sorry, that emoticon was not found"
 end
