99a46a88b84aa7a1a1127bf1fdfb7e390336df3f
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-19 03:37:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,11 @@
 # require modules here
-
+require 'pry'
+require 'yaml'
 def load_library(yml)
   emoticons = YAML.load_file(yml) #declare emoticons as variable for YAML file path
 
   new_hash = {} #method needs to create new hash
-  
+
   emoticons.each do |emotion, emoticon| #iterates over emoticons, emotion as key and emoticon as value
 
     new_hash[emotion] = {   #update empty new hash as
